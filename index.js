611a3537929524796e756584f438c19f13692703
RaphMt <rafa.mtorres117@outlook.com>
2023-12-11 01:19:09 -0300

    added verification to git repository, questions about language and project name

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-const inquirer = require("inquirer");
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { existsSync } from "fs";
+
+let projectName, lang;
+
+const sleep = (ms = 3500) => new Promise((r) => setTimeout(r, ms));
+
+async function verifyGit() {
+  if (existsSync(".git")) {
+    console.log("Already a git repository.");
+    process.exit(1);
+  } else {
+    await start();
+  }
+}
+
+async function start() {
+  const tittle = chalkAnimation.neon("Ginit");
+  await sleep();
+
+  console.log(`
+${chalk.magenta("How to use: ")}
+You will be asked about this project, and based 
+on these responses, a README will be generated.
+`);
+  tittle.stop();
+}
+
+async function askProjectName() {
+  const answer = await inquirer.prompt({
+    name: "project_name",
+    type: "input",
+    message: "What is the project name?",
+    default() {
+      return "unnamed-project";
+    },
+  });
+  projectName = answer.project_name;
+}
+
+async function askLanguage() {
+  const answer = await inquirer.prompt({
+    name: "language",
+    type: "list",
+    message: "What language will be used in this project?",
+    choices: [
+      "Javascript",
+      "Typescript",
+      "Bash",
+      "Go",
+      new inquirer.Separator(),
+      "Other",
+    ],
+  });
+  lang = answer.language;
+}
+
+// await verifyGit();
+await start();
+await askProjectName();
+await askLanguage();
