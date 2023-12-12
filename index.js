7486cde837ccfd06ff101f748730a3d4b55ff8fd
RaphMt <rafa.mtorres117@outlook.com>
2023-12-12 13:15:50 -0300

    Main logic applied, creating project in a new directory and custom readme

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
-import { existsSync, writeFile, copyFile } from "fs";
-import { type } from "os";
+import gradient from "gradient-string";
+import { createSpinner } from "nanospinner";
+import { mkdirSync, copyFile, appendFile } from "fs";
+import figlet from "figlet";
+
+import { mainMessage } from "./content/files/mainFiles.js";
 
 let projectName, lang, readme;
 
-const sleep = (ms = 3500) => new Promise((r) => setTimeout(r, ms));
+const sleep = (ms = 2700) => new Promise((r) => setTimeout(r, ms));
 
 async function start() {
-  const tittle = chalkAnimation.neon("\nGinit");
+  const msg = "Ginit";
+  figlet(
+    msg,
+    {
+      font: "Bloody",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    },
+    (err, data) => {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      } else {
+        console.log(gradient.passion.multiline(data));
+      }
+    },
+  );
   await sleep();
-
-  console.log(`
-${chalk.magenta("How to use: ")}
-You will be asked about this project, and based 
-on these responses, a README will be generated.
+  console.log(`${chalk.magenta("How to use: ")}
+Answer the next questions about the
+project you are creating, and we will
+take care of the rest.
 `);
-  tittle.stop();
-}
-
-async function verifyGit() {
-  if (existsSync(".git")) {
-    console.log("Already a git repository.");
-    process.exit(1);
-  } else {
-    await start();
-  }
 }
 
 async function askProjectName() {
@@ -65,21 +77,116 @@ async function askReadme() {
     message: "Would you like to create a README.md?",
   });
   readme = answer.readme;
+}
+
+async function createProject() {
+  const spinner = createSpinner("Putting your files together...").start();
+
+  // Creating project folder and main file for each language
+  switch (lang) {
+    case "Node.js":
+      await sleep();
+      mkdirSync(`${projectName}`);
+      appendFile(`${projectName}/index.js`, `${mainMessage.Node}`, (err) => {
+        if (err) {
+          throw err;
+        }
+        spinner.success({
+          text: "Project created.",
+        });
+      });
+      break;
+    case "Go":
+      await sleep();
+      mkdirSync(`${projectName}`);
+      appendFile(`${projectName}/main.go`, `${mainMessage.Go}`, (err) => {
+        if (err) {
+          throw err;
+        }
+        spinner.success({
+          text: "Project created.",
+        });
+      });
+      break;
+    case "Typescript":
+      await sleep();
+      mkdirSync(`${projectName}`);
+      appendFile(
+        `${projectName}/index.ts`,
+        `${mainMessage.Typescript}`,
+        (err) => {
+          if (err) {
+            throw err;
+          }
+          spinner.success({
+            text: "Project created.",
+          });
+        },
+      );
+      break;
+    case "Bash":
+      await sleep();
+      mkdirSync(`${projectName}`);
+      appendFile(`${projectName}/script.sh`, `${mainMessage.Bash}`, (err) => {
+        if (err) {
+          throw err;
+        }
+        spinner.success({
+          text: "Project created.",
+        });
+      });
+      break;
+    case "Other":
+      await sleep();
+      mkdirSync(`${projectName}`);
+      appendFile(`${projectName}/foo.sh`, `${mainMessage.Bash}`, (err) => {
+        if (err) {
+          throw err;
+        }
+        spinner.success({
+          text: "Project created.",
+        });
+      });
+      break;
+
+    default:
+      await sleep();
+      mkdirSync(`${projectName}`);
+      appendFile(
+        `${projectName}/error.md`,
+        "Something went wrong here...",
+        (err) => {
+          if (err) {
+            throw err;
+          }
+          spinner.success({
+            text: "Project directory created.",
+          });
+        },
+      );
+      break;
+  }
+  // Creating README based on users choice
   if (readme == true) {
-    copyFile(`./content/${lang}-readme.md`, "README.md", (err) => {
-      if (err) {
-        console.log("Error while creating README: ", err);
-      } else {
-        console.log("README created.");
-      }
-    });
-  } else {
-    console.log("next step");
+    copyFile(
+      `./content/readmes/${lang}-readme.md`,
+      `${projectName}/README.md`,
+      (err) => {
+        if (err) {
+          console.log("Error while creating README: ", err);
+        } else {
+          spinner.success({
+            text: "README.md created.",
+          });
+        }
+      },
+    );
   }
 }
 
-// await verifyGit();
 await start();
-// await askProjectName();
+await askProjectName();
 await askLanguage();
 await askReadme();
+
+await createProject();
