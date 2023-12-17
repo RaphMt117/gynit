6696b65e8c68b54b558ebd6f8e69b7ec360d99e3
RaphMt <rafa.mtorres117@outlook.com>
2023-12-17 15:46:33 -0300

    Updated node readme and added images

--- content/mainContent.js
+++ content/mainContent.js
@@ -12,9 +12,69 @@ func main() {
 };
 
 export const readmeContent = {
-  Node: `# Node Readme Template
+  Node: `<div align="center">
+  <img src="https://raw.githubusercontent.com/catppuccin/catppuccin/main/assets/logos/exports/1544x1544_circle.png" width="100" alt="Logo"/><br/>
+  <h1 align="center">
+    projectName
+  </h1>
+</div>
 
-some content lalala
+<p align="center">
+  <!-- Badges from [Shields.io](https://img.shields.io/)  -->
+  <a href="https://github.com/{user}/{project}"><img src="https://img.shields.io/github/languages/top/{user}/{project}?colorA=363a4f&colorB=f5a97f&style=for-the-badge"></a>
+  <a href="https://github.com/{user}/{project}"><img src="https://img.shields.io/github/repo-size/{user}/{project}?colorA=363a4f&colorB=b7bdf8&style=for-the-badge"></a>
+  <a href="https://github.com/{user}/{project}/commits"><img src="https://img.shields.io/github/commit-activity/m/{user}/{project}?colorA=363a4f&colorB=a6da95&style=for-the-badge"></a>
+</p>
+
+## What is it?
+
+-
+-
+-
+
+## Showcase
+
+<p align="center">
+  <img src="path/to/sreenshot"/>
+</p>
+
+## Usage
+
+1. Make sure you have node.js installed:
+
+\`\`\`bash
+node --version
+\`\`\`
+
+2. Run the following command:
+
+\`\`\`bash
+npx { projectName }
+\`\`\`
+
+> NPX allows you to run and use packages without having to install them locally or globally.
+> You can learn more [here](https://www.npmjs.com/package/npx)
+
+## specificFuntionality
+
+<p align="center">
+  <img src="path/to/sreenshot"/>
+  <img src="path/to/sreenshot"/>
+  <img src="path/to/sreenshot"/>
+</p>
+
+## 🙋 FAQ
+
+<!-- - Q: **"What is \`npx\`?"** -->
+
+  <!-- NPX is a tool included in NPM, and allows you to run and use packages without needing to locally or globally install them. -->
+  <!-- If a package is installed while running NPM executables with NPX, NPX will look for the package binaries (either locally or globally) and then run the package. -->
+  <!-- Your globals remain clean in this way. As a result, you can run a package only when necessary and conserve disk space. In addition, you may test packages without having to install them, which is another benefit. -->
+
+&nbsp;
+
+<p align="center"><a href="https://github.com/path/to/license"><img src="https://img.shields.io/static/v1.svg?style=for-the-badge&label=License&message={license}&logoColor=d9e0ee&colorA=363a4f&colorB=b7bdf8"/></a></p>
+
 `,
   Typescript: `# Typescript Readme Template
 
